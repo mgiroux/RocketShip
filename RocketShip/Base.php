6ba3d1a069b6203d219b0c6d17e69d545a52dbd8
Marc Giroux <marc@merchlar.com>
2015-08-04 10:45:35 -0400

    Added removeFromArray helper method

--- RocketShip/Base.php
+++ RocketShip/Base.php
@@ -16,4 +16,27 @@ class Base
     {
         $this->app = Application::$instance;
     }
+    
+    /**
+     *
+     * Remove an element from an array and return the new array, reset the array if requested
+     *
+     * @param   array   the array to edit
+     * @param   mixed   the key to remove
+     * @param   bool    reset the array (more for numerical arrays) (optional)
+     * @return  array
+     * @access  protected
+     * @final
+     *
+     */
+    final protected function removeFromArray($array, $index, $reset=true)
+    {
+        unset($array[$index]);
+
+        if ($reset) {
+            return array_values($array);
+        }
+
+        return $array;
+    }
 }

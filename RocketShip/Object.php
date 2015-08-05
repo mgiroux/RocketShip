1e19dcbeba49253980ca2f27d668c94e2908e7b7
Marc Giroux <marc@merchlar.com>
2015-08-05 11:26:54 -0400

    Changed the way the arrays are looked at
    Checking if key 0 existed was wrong, now looking at it differently, leaving numeric arrays intact and changing associative into object

--- RocketShip/Object.php
+++ RocketShip/Object.php
@@ -8,7 +8,13 @@ class Object
 
         foreach ($array as $k => $v) {
             if (is_array($v)) {
-                $this->{$k} = new Object($v);
+                $assoc = array_keys($v) !== range(0, count($v) - 1);
+
+                if (!$assoc) {
+                    $this->{$k} = $v;
+                } else {
+                    $this->{$k} = new Object($v);
+                }
             } else {
                 $this->{$k} = $v;
             }
@@ -18,7 +24,13 @@ class Object
     public function addValue($key, $value)
     {
         if (is_array($value)) {
-            $this->{$key} = new Object($value);
+            $assoc = array_keys($value) !== range(0, count($value) - 1);
+
+            if (!$assoc) {
+                $this->{$key} = $value;
+            } else {
+                $this->{$key} = new Object($value);
+            }
         } else {
             $this->{$key} = $value;
         }

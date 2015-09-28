4ee0a10be57a3c8b762527b1a0b2153dd05a6bc1
Marc Giroux <marc@merchlar.com>
2015-09-27 21:33:10 -0400

    Added metas method

--- RocketShip/Helpers/HTML.php
+++ RocketShip/Helpers/HTML.php
@@ -538,4 +538,67 @@ class HTML extends Base
             }
         }
     }
+    
+        /**
+     *
+     * Add a bunch of meta tags and basic elements to the head tag
+     *
+     * @param   object  title, description and image
+     * @param   string  the site title
+     * @access  public
+     *
+     */
+    public function metas($metas, $siteTitle='')
+    {
+        echo '<meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">';
+        echo "\r\n";
+
+        if (empty($metas->title)) {
+            echo '<title>' . $siteTitle . '</title>';
+        } else {
+            echo ' <title>'. $metas->title . ' - ' . $siteTitle . '</title>';
+        }
+        echo "\r\n";
+
+        echo '<meta name="description" content="' . $metas->description . '" />';
+        echo "\r\n";
+        echo '<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+        <link rel="shortcut icon" href="/public/app/images/favicon.ico" type="image/x-icon">
+        <link rel="icon" href="/public/app/images/favicon.ico" type="image/x-icon">
+        <link rel="apple-touch-icon" sizes="57x57" href="/public/app/images/favicons/apple-icon-57x57.png">
+        <link rel="apple-touch-icon" sizes="60x60" href="/public/app/images/favicons/apple-icon-60x60.png">
+        <link rel="apple-touch-icon" sizes="72x72" href="/public/app/images/favicons/apple-icon-72x72.png">
+        <link rel="apple-touch-icon" sizes="76x76" href="/public/app/images/favicons/apple-icon-76x76.png">
+        <link rel="apple-touch-icon" sizes="114x114" href="/public/app/images/favicons/apple-icon-114x114.png">
+        <link rel="apple-touch-icon" sizes="120x120" href="/public/app/images/favicons/apple-icon-120x120.png">
+        <link rel="apple-touch-icon" sizes="144x144" href="/public/app/images/favicons/apple-icon-144x144.png">
+        <link rel="apple-touch-icon" sizes="152x152" href="/public/app/images/favicons/apple-icon-152x152.png">
+        <link rel="apple-touch-icon" sizes="180x180" href="/public/app/images/favicons/apple-icon-180x180.png">
+        <link rel="icon" type="image/png" sizes="192x192"  href="/public/app/images/favicons/android-icon-192x192.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/public/app/images/favicons/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="96x96" href="/public/app/images/favicons/favicon-96x96.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/public/app/images/favicons/favicon-16x16.png">
+        <link rel="manifest" href="/public/app/images/favicons/manifest.json">
+        <meta name="msapplication-TileColor" content="#ffffff">
+        <meta name="msapplication-TileImage" content="/public/app/images/favicons/ms-icon-144x144.png">
+        <meta name="theme-color" content="#ffffff">
+        <meta property="og:type" content="website" />';
+        echo "\r\n";
+
+        if (empty($metas->title)) {
+            echo '<meta property="og:title" content="' . $siteTitle . '" />';
+        } else {
+            echo '<meta property="og:title" content="' . $metas->title . ' - ' . $siteTitle . '" />';
+        }
+
+        echo '<meta property="og:url" content="' . $this->app->site_url . $this->app->uri . '" />';
+
+        if (!empty($metas->image)) {
+            echo '<meta property="og:image" content="' . $this->app->site_url .  $metas->image . '" />';
+        }
+
+        echo '<meta property="og:description" content="' . $metas->description . '" />';
+        echo '<link rel="canonical" href="' . $this->app->site_url . $this->app->uri . '" />';
+    }
 }
